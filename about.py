2933fd3e0346267d16c3d75670cef5f02e009334
Gurudev <1757254+gurudevrao@users.noreply.github.com>
2018-06-08 19:03:45 +0530

    version 0.1

---
+++ about.py
@@ -0,0 +1,14 @@
+# inspired from:
+
+# https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+
+__title__ = 'gurunudi'
+__version__ = '0.1'
+__summary__ = 'The official Python client for Gurunudi AI API'
+__uri__ = 'https://gurulaghu.com/'
+__author__ = 'GuruLaghu Technologies'
+__email__ = 'gurulaghu@use.startmail.com'
+__license__ = 'MIT'
+__title__ = "gurunudi"
+__release__ = True
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='gurunudi',
+      version='0.1',
+      description='The official Python client for Gurunudi AI API',
+      url='http://github.com/gurulaghu/gurunudi',
+      author='GuruLaghu Technologies',
+      author_email='gurulaghu@use.startmail.com',
+      license='MIT',
+      packages=['gurunudi'])
