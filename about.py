f8a009b604f1f4978fa3b0d35e15ab1dc1c00901
Gurudev <1757254+gurudevrao@users.noreply.github.com>
2019-10-08 14:50:34 +0530

    release 1.3.8

--- about.py
+++ about.py
@@ -4,11 +4,11 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __title__ = 'gurunudi'
-__version__ = '1.3.7'
+__version__ = '1.3.8'
 __summary__ = 'The official Python client for Gurunudi AI API'
-__uri__ = 'https://gurulaghu.com/'
-__author__ = 'GuruLaghu Technologies'
-__email__ = 'gurulaghu@use.startmail.com'
+__uri__ = 'https://guruyuga.com/'
+__author__ = 'GuruYuga'
+__email__ = 'guruyuga@use.startmail.com'
 __license__ = 'MIT License'
 __title__ = "gurunudi"
 __release__ = True
