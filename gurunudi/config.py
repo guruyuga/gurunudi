984e4525ef698eaeaee4420b0a2bfad769522691
Gurudev <1757254+gurudevrao@users.noreply.github.com>
2020-08-05 21:28:07 +0530

    update client version to 1.4.1

--- gurunudi/config.py
+++ gurunudi/config.py
@@ -4,6 +4,6 @@
 API_URL = "https://api.gurunudi.com/{}/{}/"	
 
 """Gurunudi AI API accepts and sents JSON content only"""
-HEADERS = {'Content-type': 'application/json; charset=utf-8', 'Accept': 'application/json','User-Agent':'Gurunudi Python Client 1.4.0','GNAPI':'demo'}
+HEADERS = {'Content-type': 'application/json; charset=utf-8', 'Accept': 'application/json','User-Agent':'Gurunudi Python Client 1.4.1','GNAPI':'demo'}
 
 DEBUG = False
