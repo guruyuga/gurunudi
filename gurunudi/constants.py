0e60a9ea74c52851383d19934eb8c37a2ca6a340
Gurudev <1757254+gurudevrao@users.noreply.github.com>
2021-10-15 18:01:37 +0530

    Update constants.py

--- gurunudi/constants.py
+++ gurunudi/constants.py
@@ -3,31 +3,7 @@
 """
 List of APIs supported by Gurunudi
 """
-API_AUTOCORRECT		='autocorrect'			#checks any spelling mistakes in a text document
-API_AUTOCOMPLETE	='autocomplete'			#tries to autocomplete given text
-API_LANGUAGE		='language'			#detects the language of a text document
-API_SENTIMENT		='sentiment'			#analyzes the sentiment of a text document
-API_COREF		='coref'			#resolves co-references in a text document
-API_NAMED_ENTITIES	='named_entities'		#extracts named-entities in a text document
-API_CHAT		='chat'				#real time conversation and knowledge q&a - ideal for chatbots
-API_CLASSIFY		='classify'			#returns classification labels for given text using given model
-API_DEFINE		='define'			#returns definition of word or noun or phrase
-API_CONTEXTQA		='contextqa'			#answers based on given context
-API_FIX_CASE		='fixcase'			#returns text after fixing any incorrect case issues
-API_GRAPH_QUERY		='graph_query'			#queries gurunudi knowledge graph in a structured way
-API_NLG			='nlg'				#generates natural language text from given intent
-API_NLI			='inferences'			#generates inferences based on given text
-API_NLP			='nlp'				#sentence extraction + pos + dep + ner
-API_QUERY		='query'			#answers natural language query using gurunudi knowledge graph
-API_KEYWORDS		='keywords'			#extracts keywords from a text document
-API_SENTENCES		='sentences'			#extracts sentences in a text document
-API_INTENT		='intent'			#extracts intent of a text document
-API_SYNTAX		='syntax'			#analyzes syntax of a text document
-API_DEPENDENCY		='dependency'			#creates syntactic dependency tree of a text document
-API_TOPICS		='topics'			#returns a list of topics associated with the document
-API_SUMMARY		='summary'			#returns short summary of a large text document
-API_TRANSLATE		='translate'			#translates from source language to destination language
-API_TITLE		='title'			#returns title for a large text
+API_CHAT		='chatbot' #real time conversation and knowledge q&a - ideal for chatbots
 
 """
 Fields returned in response JSON by Gurunudi API calls and Fields sent in request JSON to Gurunudi API calls
@@ -43,4 +19,3 @@ FIELD_MODEL="model"
 ERROR_SERVER_INACCESSIBLE="server_inaccessible"
 ERROR_INVALID_LANGUAGE_CODE="invalid_language_code" 
 ERROR_NO_RESPONSE="no_response_for_api"
-
