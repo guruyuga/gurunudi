072092038aa21103cacb6b99d08508bd7e9b3ef6
Gurudev <1757254+gurudevrao@users.noreply.github.com>
2021-10-15 18:01:12 +0530

    Update config.py

--- gurunudi/config.py
+++ gurunudi/config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 """Generic URI of the Gurunudi AI API"""
-API_URL = "https://api.gurunudi.com/{}/{}/"	
+API_URL = "https://api.gurunudi.com/{}/{}"	
 
 """Gurunudi AI API accepts and sents JSON content only"""
-HEADERS = {'Content-type': 'application/json; charset=utf-8', 'Accept': 'application/json','User-Agent':'Gurunudi Python Client 1.4.1','GNAPI':'demo'}
+HEADERS = {'Content-type': 'application/json; charset=utf-8', 'Accept': 'application/json','User-Agent':'Gurunudi Python Client 2.0.3','gnapi':'demo'}
 
 DEBUG = False
